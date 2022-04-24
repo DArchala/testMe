778f66b425188bb797fab4a7a45dcbeff429b168
damian <damianarchala@gmail.com>
2022-04-24 18:34:59 +0200

    first commit

---
+++ app-angular/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import {AppRoutingModule} from "./app-routing.module";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/test/java/pl/archala/testme/TestMeApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.archala.testme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
