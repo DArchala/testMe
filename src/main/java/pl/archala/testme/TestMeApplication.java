e22aa3adec4569e90a8b02d362b354c94cfdd609
damian <damianarchala@gmail.com>
2022-04-24 18:34:59 +0200

    first commit

---
+++ src/main/java/pl/archala/testme/TestMeApplication.java
@@ -0,0 +1,13 @@
+package pl.archala.testme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestMeApplication.class, args);
+    }
+
+}
