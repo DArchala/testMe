9d176f1ae2e27c2a1802d7dd57061d606a3bb0a5
damian <damianarchala@gmail.com>
2022-05-10 12:49:57 +0200

    Replace if-statement to short version

--- src/main/java/pl/archala/testme/service/QuestionService.java
+++ src/main/java/pl/archala/testme/service/QuestionService.java
@@ -2,6 +2,7 @@ package pl.archala.testme.service;
 
 import org.springframework.stereotype.Service;
 import pl.archala.testme.models.Question;
+import pl.archala.testme.models.Questionable;
 import pl.archala.testme.models.questionTypes.MultipleChoiceQuestion;
 import pl.archala.testme.models.questionTypes.ShortAnswerQuestion;
 import pl.archala.testme.models.questionTypes.SingleChoiceQuestion;
@@ -24,23 +25,8 @@ public class QuestionService {
         this.examRepo = examRepo;
     }
 
-    public int countQuestionPoints(Question userQuestion) {
-        assert userQuestion.getId() != null : "question id is null";
-
-        if(userQuestion instanceof SingleChoiceQuestion) {
-            var userQuest = (SingleChoiceQuestion) userQuestion;
-            var templateQuest = questionRepo.findById(userQuest.getId()).orElseThrow();
-            return userQuest.countPoints(templateQuest);
-
-        } else if(userQuestion instanceof MultipleChoiceQuestion) {
-            var userQuest = (MultipleChoiceQuestion) userQuestion;
-            var templateQuest = questionRepo.findById(userQuest.getId()).orElseThrow();
-            return userQuest.countPoints(templateQuest);
-
-        } else if(userQuestion instanceof ShortAnswerQuestion) {
-            var userQuest = (ShortAnswerQuestion) userQuestion;
-            return userQuest.countPoints(null);
-        }
-        return 0;
+    public int countQuestionPoints(Questionable userQuestion) {
+        var templateQuest = questionRepo.findById(userQuestion.getId()).orElseThrow();
+        return userQuestion.countPoints(templateQuest);
     }
 }
