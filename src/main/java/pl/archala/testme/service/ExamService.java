34b1c20b615def56c16474fbd623f39d0b2d348b
damian <damianarchala@gmail.com>
2022-05-10 12:26:00 +0200

    Delete comments and unused imports

--- src/main/java/pl/archala/testme/service/ExamService.java
+++ src/main/java/pl/archala/testme/service/ExamService.java
@@ -3,8 +3,6 @@ package pl.archala.testme.service;
 import org.springframework.stereotype.Service;
 import pl.archala.testme.models.Exam;
 import pl.archala.testme.models.Question;
-import pl.archala.testme.repositories.ExamRepository;
-import pl.archala.testme.repositories.QuestionRepository;
 
 @Service
 public class ExamService {
@@ -16,14 +14,12 @@ public class ExamService {
     }
 
     public int countUserExamPoints(Exam userExam) {
+
         int points = 0;
         assert userExam.getId() != null : "userExam id is null";
 
-        // do a loop on every question filled by user
         for (Question q : userExam.getQuestions()) {
             assert q.getId() != null : "question id is null";
-
-            // przesyłamy tylko pytanie przysłane od użytkownika
             points += questionService.countQuestionPoints(q);
         }
         return points;
