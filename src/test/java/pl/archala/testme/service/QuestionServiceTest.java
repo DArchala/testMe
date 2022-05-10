a28b51b2c47f79cdbb1440d628001566efccca42
damian <damianarchala@gmail.com>
2022-05-10 15:33:07 +0200

    QuestionServiceTest zero points for ShortAnswerQuestion variant add

--- src/test/java/pl/archala/testme/service/QuestionServiceTest.java
+++ src/test/java/pl/archala/testme/service/QuestionServiceTest.java
@@ -322,4 +322,35 @@ class QuestionServiceTest {
 
     }
 
+    @Test
+    void shouldReturnZeroPointsForShortAnswerQuestion() {
+        //given
+        Answer a1 = new Answer();
+        a1.setId(1L);
+        a1.setContent("Pies");
+        a1.setCorrectness(true);
+
+        Answer a2 = new Answer();
+        a2.setId(2L);
+        a2.setContent("Kot");
+        a2.setCorrectness(true);
+
+        List<Answer> answersTemplate = new ArrayList<>(Arrays.asList(a1, a2));
+
+        var shortAnswerQuestion = new ShortAnswerQuestion(
+                "Dwa najczęstsze domowe zwierzęta:",
+                answersTemplate,
+                "nie mam pojecia"
+        );
+        shortAnswerQuestion.setId(1L);
+
+        //when
+        // In this variant we don't need question template, so we can pass any ShortAnswerQuestion
+        when(questionRepo.findById(1L)).thenReturn(Optional.of(new ShortAnswerQuestion()));
+
+        //then
+        assertEquals(0, questionService.countQuestionPoints(shortAnswerQuestion));
+
+    }
+
 }
