cc2db60d03fd30aa52613458b0c9f9824b4cb17c
damian <damianarchala@gmail.com>
2022-05-10 14:25:57 +0200

    QuestionService and QuestionServiceTest change
    Remove QuestionServiceTest unnecessary answerRepo and examRepo fields
    
    Remove QuestionService unnecessary answerRepo and examRepo fields, and
    from constructor

--- src/main/java/pl/archala/testme/service/QuestionService.java
+++ src/main/java/pl/archala/testme/service/QuestionService.java
@@ -1,13 +1,7 @@
 package pl.archala.testme.service;
 
 import org.springframework.stereotype.Service;
-import pl.archala.testme.models.Question;
 import pl.archala.testme.models.Questionable;
-import pl.archala.testme.models.questionTypes.MultipleChoiceQuestion;
-import pl.archala.testme.models.questionTypes.ShortAnswerQuestion;
-import pl.archala.testme.models.questionTypes.SingleChoiceQuestion;
-import pl.archala.testme.repositories.AnswerRepository;
-import pl.archala.testme.repositories.ExamRepository;
 import pl.archala.testme.repositories.QuestionRepository;
 
 @Service
@@ -15,14 +9,8 @@ public class QuestionService {
 
     private final QuestionRepository questionRepo;
 
-    private final AnswerRepository answerRepo;
-
-    private final ExamRepository examRepo;
-
-    public QuestionService(QuestionRepository questionRepo, AnswerRepository answerRepo, ExamRepository examRepo) {
+    public QuestionService(QuestionRepository questionRepo) {
         this.questionRepo = questionRepo;
-        this.answerRepo = answerRepo;
-        this.examRepo = examRepo;
     }
 
     public int countQuestionPoints(Questionable userQuestion) {
---
+++ src/test/java/pl/archala/testme/service/QuestionServiceTest.java
@@ -0,0 +1,73 @@
+package pl.archala.testme.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+import pl.archala.testme.models.Answer;
+import pl.archala.testme.models.questionTypes.MultipleChoiceQuestion;
+import pl.archala.testme.repositories.QuestionRepository;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@MockitoSettings(strictness = Strictness.STRICT_STUBS)
+@ExtendWith(MockitoExtension.class)
+class QuestionServiceTest {
+
+    @InjectMocks
+    private QuestionService questionService;
+
+    @Mock
+    private QuestionRepository questionRepo;
+
+    @BeforeEach
+    void setUp() {
+        questionService = new QuestionService(questionRepo);
+    }
+
+    @Test
+    void shouldReturnTwoPointsForMultipleChoiceQuestion() {
+        //given
+        Answer a1 = new Answer();
+        a1.setId(1L);
+        a1.setContent("Pies");
+        a1.setCorrectness(true);
+
+        Answer a2 = new Answer();
+        a2.setId(2L);
+        a2.setContent("Kot");
+        a2.setCorrectness(true);
+
+        Answer a3 = new Answer();
+        a3.setId(3L);
+        a3.setContent("Ser");
+        a3.setCorrectness(false);
+        List<Answer> answersFromUser = new ArrayList<>(Arrays.asList(a1, a2, a3));
+
+        var multipleChoiceQuestion = new MultipleChoiceQuestion();
+        multipleChoiceQuestion.setId(1L);
+        multipleChoiceQuestion.setContent("Wybierz zwierzęta:");
+        multipleChoiceQuestion.setAnswers(answersFromUser);
+
+        var multipleChoiceQuestionTemplate = new MultipleChoiceQuestion();
+        multipleChoiceQuestion.setId(1L);
+        multipleChoiceQuestion.setContent("Wybierz zwierzęta:");
+        multipleChoiceQuestion.setAnswers(answersFromUser);
+
+        //when
+        when(questionRepo.findById(1L).orElseThrow()).thenReturn(multipleChoiceQuestionTemplate);
+
+        //then
+        assertEquals(2, questionService.countQuestionPoints(multipleChoiceQuestion));
+
+    }
+}
