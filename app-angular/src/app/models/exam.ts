9552324a271e00e11aa467850c6f4aea9e55407a
damian <damianarchala@gmail.com>
2022-05-11 17:39:02 +0200

    Change Exam angular class fields order

--- app-angular/src/app/models/exam.ts
+++ app-angular/src/app/models/exam.ts
@@ -2,8 +2,8 @@ import {Question} from "./question";
 
 export class Exam {
   id: number | any;
-  difficultyLevel!: string;
-  examName: string | any;
   questions: Question[] | any;
+  examName: string | any;
+  difficultyLevel!: string;
   timeInSeconds!: number;
 }
