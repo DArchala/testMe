7b2a06663d8327b717eb31f54ab8cfef3a06159d
damian <damianarchala@gmail.com>
2022-05-13 10:36:41 +0200

    Change ExamService java
    Add private final ExamRepository field
    
    Update examService constructor
    
    Add getMaxPossibleExamPoints method

--- src/main/java/pl/archala/testme/service/ExamService.java
+++ src/main/java/pl/archala/testme/service/ExamService.java
@@ -3,14 +3,20 @@ package pl.archala.testme.service;
 import org.springframework.stereotype.Service;
 import pl.archala.testme.models.Exam;
 import pl.archala.testme.models.Question;
+import pl.archala.testme.repositories.ExamRepository;
+
+import java.util.Optional;
 
 @Service
 public class ExamService {
 
     private final QuestionService questionService;
 
-    public ExamService(QuestionService questionService) {
+    private final ExamRepository examRepo;
+
+    public ExamService(QuestionService questionService, ExamRepository examRepository) {
         this.questionService = questionService;
+        this.examRepo = examRepository;
     }
 
     public int countUserExamPoints(Exam userExam) {
@@ -20,6 +26,15 @@ public class ExamService {
             points += questionService.countQuestionPoints(q);
         
         return points;
+    }
+
+    public long getMaxPossibleExamPoints(Long examId) {
+        Optional<Exam> exam = examRepo.findById(examId);
+        long counter = 0;
+        for(Question q : exam.orElseThrow().getQuestions()) {
+            counter += q.countCorrectAnswers();
+        }
+        return counter;
     }
 
 }
