d64910e06b37baeec7a92b9c75f7b3b5c9128c1d
damian <damianarchala@gmail.com>
2022-05-16 18:36:24 +0200

    Add toString method to exam.ts

--- app-angular/src/app/models/exam.ts
+++ app-angular/src/app/models/exam.ts
@@ -6,4 +6,12 @@ export class Exam {
   examName!: string;
   difficultyLevel!: string;
   timeInSeconds!: number;
+
+  toString(){
+    return "id=" + this.id
+      +", questions=" + this.questions
+      +", examName=" + this.examName
+      +", difficultyLevel=" + this.difficultyLevel
+      +", timeInSeconds=" + this.timeInSeconds;
+  }
 }
