b5f09f4a7aad169aae0ff39f2bf5bed8c1e8ed70
damian <damianarchala@gmail.com>
2022-05-16 18:51:21 +0200

    Overriding in short-answer-question.ts
    Add overrided fields
    
    Add overrided all args constructor

--- app-angular/src/app/models/questionTypes/short-answer-question.ts
+++ app-angular/src/app/models/questionTypes/short-answer-question.ts
@@ -1,6 +1,19 @@
 import {Question} from "../question";
+import {Answer} from "../answer";
 
-export class ShortAnswerQuestion extends Question{
+export class ShortAnswerQuestion extends Question {
 
-  userAnswer!: String;
+  override id: any;
+  override content: string;
+  override answers: Answer[];
+  override type: string;
+  override userAnswer: string;
+
+  constructor(content: string, answers: Answer[], type: string, userAnswer: string) {
+    super(content, answers, type, userAnswer);
+    this.content = content;
+    this.answers = answers;
+    this.type = type;
+    this.userAnswer = userAnswer;
+  }
 }
