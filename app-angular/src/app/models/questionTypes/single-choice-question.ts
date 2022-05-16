3cacc290c91ab029970ff0af1c8f8a2078277b99
damian <damianarchala@gmail.com>
2022-05-16 18:56:51 +0200

    New constructor in single-choice-question.ts

--- app-angular/src/app/models/questionTypes/single-choice-question.ts
+++ app-angular/src/app/models/questionTypes/single-choice-question.ts
@@ -1,4 +1,9 @@
 import {Question} from "../question";
+import {Answer} from "../answer";
 
 export class SingleChoiceQuestion extends Question {
+
+  constructor(content: string, answers: Answer[], type: string, userAnswer: string) {
+    super(content, answers, type, userAnswer);
+  }
 }
