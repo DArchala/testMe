496199560ab2d0105e9d87e3ebd7cc16aca0728e
damian <damianarchala@gmail.com>
2022-05-16 21:32:00 +0200

    Change new-exam.component.ts
    Add displayExam method
    
    Add deleteThisQuestion method
    
    Set default question subclasses objects type value
    
    Set default field values in examModel

--- app-angular/src/app/pages/new-exam/new-exam.component.ts
+++ app-angular/src/app/pages/new-exam/new-exam.component.ts
@@ -15,33 +15,47 @@ export class NewExamComponent {
 
   constructor() {
     this.examModel = new Exam();
-    this.examModel.questions = [new ShortAnswerQuestion("Wpisz treść pierwszego pytania",
-      [new Answer(), new Answer()], "short", "")];
+    this.examModel.questions = [new SingleChoiceQuestion("",
+      [new Answer(), new Answer()], "single", "")];
+    this.examModel.id = 0;
+    this.examModel.examName = "examName";
+    this.examModel.timeInSeconds = 1800;
+    this.examModel.difficultyLevel = "MEDIUM";
   }
 
   examModel!: Exam;
   newQuestionType = "single";
 
   addNewQuestion() {
-    console.log(this.examModel.toString());
-    console.log(this.newQuestionType);
+
     let quest: any;
     switch (this.newQuestionType) {
       case "single":
-        quest = new SingleChoiceQuestion("single", [new Answer(), new Answer()], "", "");
+        quest = new SingleChoiceQuestion("", [new Answer(), new Answer()], "single", "");
         break;
       case "multiple":
-        quest = new MultipleChoiceQuestion("multiple", [new Answer(), new Answer()], "", "");
+        quest = new MultipleChoiceQuestion("", [new Answer(), new Answer()], "multiple", "");
         break;
       case "short":
-        quest = new ShortAnswerQuestion("short", [new Answer(), new Answer()], "", "");
+        quest = new ShortAnswerQuestion("", [new Answer(), new Answer()], "short", "");
         break;
     }
     this.examModel.questions.push(quest);
-    console.log(quest);
   }
 
   addNewAnswerToQuestion(question: Question) {
     question.answers.push(new Answer());
+  }
+
+  displayExam() {
+    console.log(this.examModel.toString());
+    this.examModel.questions.forEach(question => console.log(question));
+  }
+
+  deleteThisQuestion(question: Question) {
+    let index = this.examModel.questions.indexOf(question, 0);
+    if (index > -1) {
+      this.examModel.questions.splice(index, 1);
+    }
   }
 }
