67cfcf08abe26cae38dc33c51a8187a308bab4d7
damian <damianarchala@gmail.com>
2022-05-18 00:00:26 +0200

    Create NewExamController class

---
+++ src/main/java/pl/archala/testme/controllers/NewExamController.java
@@ -0,0 +1,35 @@
+package pl.archala.testme.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.archala.testme.models.Exam;
+import pl.archala.testme.models.ExamDifficultyLevel;
+import pl.archala.testme.service.ExamService;
+
+@Slf4j
+@RestController
+@RequestMapping(path = "/new-exam", produces = "application/json")
+@CrossOrigin(origins = "*")
+public class NewExamController {
+
+    private final ExamService examService;
+
+    public NewExamController(ExamService examService) {
+        this.examService = examService;
+    }
+
+    @GetMapping
+    public ResponseEntity<ExamDifficultyLevel[]> getNewExamData() {
+        return new ResponseEntity<>(ExamDifficultyLevel.values(), HttpStatus.OK);
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<Exam> saveNewExam(@RequestBody Exam exam) {
+        log.info("new exam = " + exam);
+        examService.saveNewExam(exam);
+        return new ResponseEntity<>(exam, HttpStatus.OK);
+    }
+
+}
