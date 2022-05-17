a743fe7ae8239d88d2df034dfc745fd037ac4de4
damian <damianarchala@gmail.com>
2022-05-17 12:53:04 +0200

    Add new elements in new-exam ts
    Add sample answers in initial exam question
    
    Change addNewAnswerQuestion
    
    Add deleteThisAnswer method
    
    Add uncheckOtherAnswers method
    
    Add countCorrectAnswers method
    
    Add postExam method
    
    Add validation methods

--- app-angular/src/app/pages/new-exam/new-exam.component.ts
+++ app-angular/src/app/pages/new-exam/new-exam.component.ts
@@ -15,8 +15,10 @@ export class NewExamComponent {
 
   constructor() {
     this.examModel = new Exam();
+    let sampleAnswer1 = new Answer("", false);
+    let sampleAnswer2 = new Answer("", false);
     this.examModel.questions = [new SingleChoiceQuestion("",
-      [new Answer(), new Answer()], "single", "")];
+      [sampleAnswer1, sampleAnswer2], "single", "")];
     this.examModel.id = 0;
     this.examModel.examName = "examName";
     this.examModel.timeInSeconds = 1800;
@@ -27,24 +29,31 @@ export class NewExamComponent {
   newQuestionType = "single";
 
   addNewQuestion() {
-
     let quest: any;
+    let answer = new Answer("", false);
+    let answer2 = new Answer("", false);
+
     switch (this.newQuestionType) {
       case "single":
-        quest = new SingleChoiceQuestion("", [new Answer(), new Answer()], "single", "");
+        quest = new SingleChoiceQuestion("", [answer, answer2], "single", "");
         break;
       case "multiple":
-        quest = new MultipleChoiceQuestion("", [new Answer(), new Answer()], "multiple", "");
+        quest = new MultipleChoiceQuestion("", [answer, answer2], "multiple", "");
         break;
       case "short":
-        quest = new ShortAnswerQuestion("", [new Answer(), new Answer()], "short", "");
+        answer.correctness = true;
+        quest = new ShortAnswerQuestion("", [answer], "short", "");
         break;
     }
     this.examModel.questions.push(quest);
   }
 
   addNewAnswerToQuestion(question: Question) {
-    question.answers.push(new Answer());
+    let answer = new Answer("", false);
+    if (question.type === 'short') {
+      answer.correctness = true;
+    }
+    question.answers.push(answer);
   }
 
   displayExam() {
@@ -56,6 +65,97 @@ export class NewExamComponent {
     let index = this.examModel.questions.indexOf(question, 0);
     if (index > -1) {
       this.examModel.questions.splice(index, 1);
+    }
+  }
+
+  deleteThisAnswer(question: Question, answer: Answer) {
+    let index = question.answers.indexOf(answer, 0);
+    if (index > -1) {
+      question.answers.splice(index, 1);
+    }
+  }
+
+  uncheckOtherAnswers(answer: Answer, question: Question) {
+    if (question.type === 'single') {
+      question.answers.forEach(answer => answer.correctness = false);
+      answer.correctness = true;
+    }
+  }
+
+  countCorrectAnswers(exam: Exam) {
+    let counter = 0;
+    exam.questions.forEach(question => {
+      question.answers.forEach(answer => {
+        if (answer.correctness) counter++;
+      })
+    });
+    return counter;
+  }
+
+  postExam() {
+    this.validateExamName();
+    this.validateDifficultyLevel();
+    this.validateExamTime();
+    this.validateExamQuestions();
+  }
+
+  private validateExamName() {
+    if (this.examModel.examName.length === 0) alert("Nazwa egzaminu nie może być pusta.");
+    else if (this.examModel.examName.length > 60) alert("Nazwa egzaminu nie może być dłuższa niż 60 znaków.");
+  }
+
+  private validateDifficultyLevel() {
+    if (this.examModel.difficultyLevel.length === 0) alert("Poziom trudności egzaminu nie może być pusty.");
+    else if (this.examModel.difficultyLevel.length > 60) alert("Poziom trudności egzaminu nie może być dłuższy niż 60 znaków.");
+  }
+
+  private validateExamTime() {
+    if (this.examModel.timeInSeconds < 60 || this.examModel.timeInSeconds > 86400) alert("Czas egzaminu musi wynosić od 60 do 86400 sekund.");
+  }
+
+  private validateExamQuestions() {
+    let breakAll = false;
+
+    if (this.examModel.questions.length < 1) alert("Egzamin musi zawierać conajmniej jedno pytanie.");
+
+    for (let question of this.examModel.questions) {
+
+      if (question.content.length === 0) {
+        alert("Każde pytanie musi zawierać treść.");
+        break;
+      }
+
+      if (question.answers.length === 0) {
+        alert("Każde pytanie musi zawierać conajmniej jedną odpowiedź.");
+        break;
+      }
+
+      if ((question.type === 'single' || question.type === 'multiple') && question.answers.length === 1) {
+        alert("Każde pytanie jednokrotnego lub wielokrotnego wyboru musi zawierać conajmniej dwie odpowiedzi.");
+        break;
+      }
+
+      for (let answer of question.answers) {
+
+        if (answer.content.length === 0) {
+          alert("Każda odpowiedź musi zawierać treść.");
+          breakAll = true;
+          break;
+        }
+
+        let amountOfCorrectAnswers = 0;
+        question.answers.forEach(answer => {
+          if (answer.correctness) amountOfCorrectAnswers++;
+        });
+        if (amountOfCorrectAnswers === 0) {
+          alert("Każde pytanie musi zawierać conajmniej jedną prawidłową odpowiedź.");
+          breakAll = true;
+          break;
+        }
+
+      }
+
+      if (breakAll) break;
     }
   }
 }
