ccb9fe23f0a7186b678b93273c81ae038b564a5b
damian <damianarchala@gmail.com>
2022-05-17 12:48:57 +0200

    Constructur and toString in answer.ts

--- app-angular/src/app/models/answer.ts
+++ app-angular/src/app/models/answer.ts
@@ -2,4 +2,13 @@ export class Answer {
   id: any;
   content!: string;
   correctness!: boolean;
+
+  constructor(content: string, correctness: boolean) {
+    this.content = content;
+    this.correctness = correctness;
+  }
+
+  toString() {
+    return "id=" + this.id + ", content=" + this.content + ", correctness=" + this.correctness;
+  }
 }
