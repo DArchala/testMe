057731a416a59277e61756cbab77c00b61e86257
damian <damianarchala@gmail.com>
2022-05-17 23:49:33 +0200

    Change short-answer-question.ts
    Remove overrided fields
    
    Add all args constructor

--- app-angular/src/app/models/questionTypes/short-answer-question.ts
+++ app-angular/src/app/models/questionTypes/short-answer-question.ts
@@ -3,17 +3,7 @@ import {Answer} from "../answer";
 
 export class ShortAnswerQuestion extends Question {
 
-  override id: any;
-  override content: string;
-  override answers: Answer[];
-  override type: string;
-  override userAnswer: string;
-
-  constructor(content: string, answers: Answer[], type: string, userAnswer: string) {
-    super(content, answers, type, userAnswer);
-    this.content = content;
-    this.answers = answers;
-    this.type = type;
-    this.userAnswer = userAnswer;
+  constructor(id: any, content: string, answers: Answer[], type: string, userAnswer: string) {
+    super(id, content, answers, type, userAnswer);
   }
 }
