39e4d58aa544d673d4f3f0104d9171e77e37c5eb
damian <damianarchala@gmail.com>
2022-05-18 09:59:53 +0200

    Add saveNewExam method to ExamService

--- src/main/java/pl/archala/testme/service/ExamService.java
+++ src/main/java/pl/archala/testme/service/ExamService.java
@@ -1,10 +1,15 @@
 package pl.archala.testme.service;
 
 import org.springframework.stereotype.Service;
+import pl.archala.testme.models.Answer;
 import pl.archala.testme.models.Exam;
 import pl.archala.testme.models.Question;
+import pl.archala.testme.repositories.AnswerRepository;
 import pl.archala.testme.repositories.ExamRepository;
+import pl.archala.testme.repositories.QuestionRepository;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -14,9 +19,15 @@ public class ExamService {
 
     private final ExamRepository examRepo;
 
-    public ExamService(QuestionService questionService, ExamRepository examRepository) {
+    private final AnswerRepository answerRepo;
+
+    private final QuestionRepository questionRepo;
+
+    public ExamService(QuestionService questionService, ExamRepository examRepo, AnswerRepository answerRepo, QuestionRepository questionRepo) {
         this.questionService = questionService;
-        this.examRepo = examRepository;
+        this.examRepo = examRepo;
+        this.answerRepo = answerRepo;
+        this.questionRepo = questionRepo;
     }
 
     public int countUserExamPoints(Exam userExam) {
@@ -38,6 +49,29 @@ public class ExamService {
     }
 
     public void saveNewExam(Exam exam) {
+        List<Question> questionList = new ArrayList<>();
 
+        for (Question q : exam.getQuestions()) {
+
+            List<Answer> answerList = new ArrayList<>();
+
+            for (Answer a : q.getAnswers()) {
+                a.setId(null);
+                answerList.add(a);
+            }
+
+            answerRepo.saveAll(answerList);
+
+            q.setId(null);
+            q.setAnswers(answerList);
+            questionList.add(q);
+        }
+
+        questionRepo.saveAll(questionList);
+
+        exam.setId(null);
+        exam.setQuestions(questionList);
+
+        examRepo.save(exam);
     }
 }
