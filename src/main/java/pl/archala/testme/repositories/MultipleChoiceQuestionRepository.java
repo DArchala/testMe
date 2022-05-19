17a6577cc8c289fe73d09437027841010fcd574f
damian <damianarchala@gmail.com>
2022-05-19 14:06:36 +0200

    Remove unused Question subclasses repositories

--- src/main/java/pl/archala/testme/repositories/MultipleChoiceQuestionRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.archala.testme.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import pl.archala.testme.models.questionTypes.MultipleChoiceQuestion;
-
-@Repository
-public interface MultipleChoiceQuestionRepository extends CrudRepository<MultipleChoiceQuestion, Long> {
-}
--- src/main/java/pl/archala/testme/repositories/ShortAnswerQuestionRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.archala.testme.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import pl.archala.testme.models.questionTypes.ShortAnswerQuestion;
-
-@Repository
-public interface ShortAnswerQuestionRepository extends CrudRepository<ShortAnswerQuestion, Long> {
-}
