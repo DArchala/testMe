5da917fe9887e3fb09e37fea48250a67a26408f3
damian <damianarchala@gmail.com>
2022-05-21 10:48:38 +0200

    Change toString in MultipleChoiceQuestion

--- src/main/java/pl/archala/testme/entity/questionTypes/MultipleChoiceQuestion.java
+++ src/main/java/pl/archala/testme/entity/questionTypes/MultipleChoiceQuestion.java
@@ -18,15 +18,15 @@ import java.util.List;
 @JsonTypeName("multiple")
 public class MultipleChoiceQuestion extends Question {
 
+    public MultipleChoiceQuestion() {
+    }
+
     public MultipleChoiceQuestion(String content, List<Answer> answers) {
         super(content, answers);
         if (answers.size() < 2)
             throw new IllegalArgumentException("Number of available answers should be greater or equal to 2.");
         if (answers.stream().noneMatch(Answer::isCorrectness))
             throw new IllegalArgumentException("Number of correct answers should be greater or equal to 1.");
-    }
-
-    public MultipleChoiceQuestion() {
     }
 
     public int countPoints(Questionable question) {
@@ -52,9 +52,9 @@ public class MultipleChoiceQuestion extends Question {
     @Override
     public String toString() {
         return "MultipleChoiceQuestion{" +
-                "id='" + getId() + '\'' +
-                ", content='" + content + '\'' +
+                "content='" + content + '\'' +
                 ", answers=" + answers +
+                ", id=" + getId() +
                 '}';
     }
 }
