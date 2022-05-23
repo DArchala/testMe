278a937409c3a8440b488c2ab1aef0ae25837fcb
damian <damianarchala@gmail.com>
2022-05-23 11:59:25 +0200

    Update ExamController
    Add methods from deleted NewExamController
    
    Add getExamToEditById method
    
    Add putExam method

--- src/main/java/pl/archala/testme/controller/ExamController.java
+++ src/main/java/pl/archala/testme/controller/ExamController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.archala.testme.entity.Exam;
+import pl.archala.testme.entity.ExamDifficultyLevel;
 import pl.archala.testme.repository.ExamRepository;
 import pl.archala.testme.service.ExamService;
 
@@ -13,7 +14,7 @@ import java.util.Optional;
 
 @Slf4j
 @RestController
-@RequestMapping(path = "/exams", produces = "application/json")
+@RequestMapping(produces = "application/json")
 @CrossOrigin(origins = "*")
 public class ExamController {
 
@@ -25,28 +26,53 @@ public class ExamController {
         this.examService = examService;
     }
 
-    @GetMapping
+    @GetMapping("/exams")
     public ResponseEntity<List<Exam>> getExams() {
         List<Exam> exams = examService.getAllExams();
         if (exams.isEmpty()) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         return new ResponseEntity<>(exams, HttpStatus.OK);
     }
 
-    @PostMapping("/exam")
+    @PostMapping("/exams/exam")
     public int checkExamCorrectness(@RequestBody Exam exam) {
         return examService.countUserExamPoints(exam);
     }
 
-    @PostMapping("/exam/{id}")
-    public int getExamMaxPoints(@PathVariable("id") Long id) {
-        return examService.getMaxPossibleExamPoints(id);
-    }
-
-    @GetMapping("/exam/{id}")
+    @GetMapping("/exams/exam/{id}")
     public ResponseEntity<Exam> getExamById(@PathVariable("id") Long id) {
         Optional<Exam> exam = examRepo.findById(id);
         if (exam.isEmpty()) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         return new ResponseEntity<>(exam.get().setAllAnswersFalse(), HttpStatus.OK);
+    }
+
+    @PostMapping("/exams/exam/{id}")
+    public int getExamMaxPoints(@PathVariable("id") Long id) {
+        return examService.getMaxPossibleExamPoints(id);
+    }
+
+    @GetMapping("/new-exam")
+    public ResponseEntity<ExamDifficultyLevel[]> getNewExamData() {
+        return new ResponseEntity<>(ExamDifficultyLevel.values(), HttpStatus.OK);
+    }
+
+    @PostMapping("/new-exam/save")
+    public ResponseEntity<Exam> saveNewExam(@RequestBody Exam exam) {
+        if (examService.saveNewExam(exam)) return new ResponseEntity<>(exam, HttpStatus.OK);
+        else return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @GetMapping("/exams/edit/{id}")
+    public ResponseEntity<Exam> getExamToEditById(@PathVariable("id") Long id) {
+        Optional<Exam> exam = examRepo.findById(id);
+        if (exam.isEmpty()) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(exam.get(), HttpStatus.OK);
+    }
+
+    @PutMapping("/exams/edit")
+    public ResponseEntity<?> putExam(@RequestBody Exam exam) {
+        log.info(exam.toString());
+        if(examService.putExam(exam)) return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
     }
 
 }
