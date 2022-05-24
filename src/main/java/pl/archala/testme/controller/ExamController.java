9b8fe6071a5d0834504530eb29f3f187a0eacbb9
damian <damianarchala@gmail.com>
2022-05-24 12:49:41 +0200

    Update ExamController response entities

--- src/main/java/pl/archala/testme/controller/ExamController.java
+++ src/main/java/pl/archala/testme/controller/ExamController.java
@@ -27,9 +27,9 @@ public class ExamController {
     }
 
     @GetMapping("/exams")
-    public ResponseEntity<List<Exam>> getExams() {
+    public ResponseEntity<?> getExams() {
         List<Exam> exams = examService.getAllExams();
-        if (exams.isEmpty()) return new ResponseEntity<>(HttpStatus.OK);
+        if (exams.isEmpty()) return new ResponseEntity<>("No exams found", HttpStatus.NOT_FOUND);
         return new ResponseEntity<>(exams, HttpStatus.OK);
     }
 
@@ -39,15 +39,18 @@ public class ExamController {
     }
 
     @GetMapping("/exams/exam/{id}")
-    public ResponseEntity<Exam> getExamById(@PathVariable("id") Long id) {
+    public ResponseEntity<?> getExamById(@PathVariable("id") Long id) {
         Optional<Exam> exam = examRepo.findById(id);
-        if (exam.isEmpty()) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        if (exam.isEmpty()) return new ResponseEntity<>("Exam not found", HttpStatus.NOT_FOUND);
         return new ResponseEntity<>(exam.get().setAllAnswersFalse(), HttpStatus.OK);
     }
 
     @PostMapping("/exams/exam/{id}")
-    public int getExamMaxPoints(@PathVariable("id") Long id) {
-        return examService.getMaxPossibleExamPoints(id);
+    public ResponseEntity<?> getExamMaxPoints(@PathVariable("id") Long id) {
+        int examMaxPoints = examService.getMaxPossibleExamPoints(id);
+        if (examMaxPoints == -1)
+            return new ResponseEntity<>("Exam not found", HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(examMaxPoints, HttpStatus.OK);
     }
 
     @GetMapping("/new-exam")
@@ -56,28 +59,28 @@ public class ExamController {
     }
 
     @PostMapping("/new-exam/save")
-    public ResponseEntity<Exam> saveNewExam(@RequestBody Exam exam) {
+    public ResponseEntity<?> saveNewExam(@RequestBody Exam exam) {
         if (examService.saveNewExam(exam)) return new ResponseEntity<>(exam, HttpStatus.OK);
-        else return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        else return new ResponseEntity<>("Saving exam failed", HttpStatus.BAD_REQUEST);
     }
 
     @GetMapping("/exams/edit/{id}")
-    public ResponseEntity<Exam> getExamToEditById(@PathVariable("id") Long id) {
+    public ResponseEntity<?> getExamToEditById(@PathVariable("id") Long id) {
         Optional<Exam> exam = examRepo.findById(id);
-        if (exam.isEmpty()) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        if (exam.isEmpty()) return new ResponseEntity<>("Exam not found", HttpStatus.NOT_FOUND);
         return new ResponseEntity<>(exam.get(), HttpStatus.OK);
     }
 
     @PutMapping("/exams/edit")
     public ResponseEntity<?> putExam(@RequestBody Exam exam) {
-        if (examService.putExam(exam)) return new ResponseEntity<>(HttpStatus.OK);
+        if (examService.putExam(exam)) return new ResponseEntity<>("Exam saved", HttpStatus.OK);
         return new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
     }
 
     @DeleteMapping("/exams/delete/{id}")
     public ResponseEntity<?> deleteExam(@PathVariable("id") Long id) {
-        if (examService.deleteExam(id)) return new ResponseEntity<>(HttpStatus.OK);
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        if (examService.deleteExam(id)) return new ResponseEntity<>("Exam deleted", HttpStatus.OK);
+        return new ResponseEntity<>("Exam not found", HttpStatus.NOT_FOUND);
     }
 
 }
