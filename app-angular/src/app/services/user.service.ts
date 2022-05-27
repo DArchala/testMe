60a6053c6b403c232f5cf7f2acc76f42a7c1af32
damian <damianarchala@gmail.com>
2022-05-27 15:11:47 +0200

    Add user.service and user.ts

---
+++ app-angular/src/app/services/user.service.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {User} from "../models/user";
+import {tap} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private url = "http://localhost:8080/api";
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  register(user: User) {
+    return this.httpClient.post<User>(this.url + `/register`, user).pipe(tap(console.log));
+  }
+
+}
