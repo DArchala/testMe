cdcb5b00da575aec9292c116747e34bef666d162
damian <damianarchala@gmail.com>
2022-05-27 15:26:50 +0200

    Add TokenService findByValue method, repo injection

--- src/main/java/pl/archala/testme/service/TokenService.java
+++ src/main/java/pl/archala/testme/service/TokenService.java
@@ -1,2 +1,21 @@
-package pl.archala.testme.service;public class TokenService {
+package pl.archala.testme.service;
+
+import org.springframework.stereotype.Service;
+import pl.archala.testme.repository.TokenRepository;
+import pl.archala.testme.security.Token;
+
+import javax.persistence.EntityNotFoundException;
+
+@Service
+public class TokenService {
+
+    private final TokenRepository tokenRepo;
+
+    public TokenService(TokenRepository tokenRepo) {
+        this.tokenRepo = tokenRepo;
+    }
+
+    public Token findByValue(String value) {
+        return tokenRepo.findByValue(value).orElseThrow(() -> new EntityNotFoundException("Token not found"));
+    }
 }
