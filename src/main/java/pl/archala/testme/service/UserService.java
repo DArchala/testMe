eece16a07da8d76b965d81c4fd1494942daa878c
damian <damianarchala@gmail.com>
2022-05-27 15:29:44 +0200

    Add UserService data

--- src/main/java/pl/archala/testme/service/UserService.java
+++ src/main/java/pl/archala/testme/service/UserService.java
@@ -1,2 +1,58 @@
-package pl.archala.testme.service;public class UserService {
+package pl.archala.testme.service;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import pl.archala.testme.entity.User;
+import pl.archala.testme.enums.RoleEnum;
+import pl.archala.testme.repository.TokenRepository;
+import pl.archala.testme.repository.UserRepository;
+import pl.archala.testme.security.Token;
+
+import javax.mail.MessagingException;
+import java.util.UUID;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepo;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final TokenRepository tokenRepo;
+
+    private final MailService mailService;
+
+    public UserService(UserRepository userRepo, PasswordEncoder passwordEncoder, TokenRepository tokenRepo, MailService mailService) {
+        this.userRepo = userRepo;
+        this.passwordEncoder = passwordEncoder;
+        this.tokenRepo = tokenRepo;
+        this.mailService = mailService;
+    }
+
+    public boolean saveUser(User user) {
+        user.setRole(RoleEnum.USER);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepo.save(user);
+        sendToken(user);
+        return true;
+    }
+
+    private void sendToken(User user) {
+        String tokenValue = UUID.randomUUID().toString();
+
+        Token token = new Token();
+        token.setValue(tokenValue);
+        token.setUser(user);
+        tokenRepo.save(token);
+
+        String url = "http://localhost:8080/api/token?value="+tokenValue;
+
+        try {
+            mailService.sendMail(user.getEmail(), "token", url, false);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 }
