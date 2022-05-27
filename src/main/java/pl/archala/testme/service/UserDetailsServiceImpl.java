9522ab7e31d92e181a35d92fbd7876787090bfc0
damian <damianarchala@gmail.com>
2022-05-27 15:27:38 +0200

    Remove PasswordEncoder Bean

--- src/main/java/pl/archala/testme/service/UserDetailsServiceImpl.java
+++ src/main/java/pl/archala/testme/service/UserDetailsServiceImpl.java
@@ -1,11 +1,8 @@
-package pl.archala.testme.entity;
+package pl.archala.testme.service;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import pl.archala.testme.repository.UserRepository;
 
@@ -13,11 +10,6 @@ import pl.archala.testme.repository.UserRepository;
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final UserRepository userRepo;
-
-    @Bean
-    public PasswordEncoder getPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
     public UserDetailsServiceImpl(UserRepository userRepo) {
         this.userRepo = userRepo;
