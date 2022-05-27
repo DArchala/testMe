0261159a52f4c7dec1a8c2398952e377d4b0864d
damian <damianarchala@gmail.com>
2022-05-27 15:23:37 +0200

    Add fields, setters,getters,annotations in Token.java

--- src/main/java/pl/archala/testme/security/Token.java
+++ src/main/java/pl/archala/testme/security/Token.java
@@ -1,2 +1,34 @@
-package pl.archala.testme.security;public class Token {
+package pl.archala.testme.security;
+
+import pl.archala.testme.entity.User;
+import pl.archala.testme.entity.abstractEntities.AbstractEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tokens")
+public class Token extends AbstractEntity<Long> {
+
+    private String value;
+
+    @OneToOne
+    private User user;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
