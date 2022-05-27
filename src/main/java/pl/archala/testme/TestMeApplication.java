ba4d3d968654d53213234202d82312d38cf81b21
damian <damianarchala@gmail.com>
2022-05-27 15:19:33 +0200

    Add UserController implementation

--- src/main/java/pl/archala/testme/TestMeApplication.java
+++ src/main/java/pl/archala/testme/TestMeApplication.java
@@ -1,26 +1,7 @@
 package pl.archala.testme;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import pl.archala.testme.entity.Answer;
-import pl.archala.testme.entity.Exam;
-import pl.archala.testme.entity.User;
-import pl.archala.testme.enums.ExamDifficultyLevel;
-import pl.archala.testme.entity.abstractEntities.Question;
-import pl.archala.testme.entity.questionTypes.MultipleChoiceQuestion;
-import pl.archala.testme.entity.questionTypes.ShortAnswerQuestion;
-import pl.archala.testme.entity.questionTypes.SingleChoiceQuestion;
-import pl.archala.testme.repository.AnswerRepository;
-import pl.archala.testme.repository.ExamRepository;
-import pl.archala.testme.repository.QuestionRepository;
-import pl.archala.testme.repository.UserRepository;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @SpringBootApplication
 public class TestMeApplication {
--- src/main/java/pl/archala/testme/controller/UserController.java
+++ src/main/java/pl/archala/testme/controller/UserController.java
@@ -1,2 +1,42 @@
-package pl.archala.testme.controller;public class UserController {
+package pl.archala.testme.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.archala.testme.entity.User;
+import pl.archala.testme.security.Token;
+import pl.archala.testme.service.TokenService;
+import pl.archala.testme.service.UserService;
+
+@Slf4j
+@RestController
+@RequestMapping(path = "/api", produces = "application/json")
+@CrossOrigin(origins = "*")
+public class UserController {
+
+    private final UserService userService;
+
+    private final TokenService tokenService;
+
+    public UserController(UserService userService, TokenService tokenService) {
+        this.userService = userService;
+        this.tokenService = tokenService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody User user) {
+        if (userService.saveUser(user)) return new ResponseEntity<>("User saved", HttpStatus.CREATED);
+        else return new ResponseEntity<>("User not saved", HttpStatus.BAD_REQUEST);
+    }
+
+    @PostMapping("/token")
+    public ResponseEntity<?> registerUser(@RequestParam String value) {
+        Token token = tokenService.findByValue(value);
+        User user = token.getUser();
+        user.setEnabled(true);
+        userService.saveUser(user);
+        return null;
+    }
+
 }
