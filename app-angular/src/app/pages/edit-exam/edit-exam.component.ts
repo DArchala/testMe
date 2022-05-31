718549a66ef46d79d6f48a6580d20f2002fb014e
damian <damianarchala@gmail.com>
2022-05-31 15:04:10 +0200

    Update edit-exam according to new-exam component

--- app-angular/src/app/pages/edit-exam/edit-exam.component.ts
+++ app-angular/src/app/pages/edit-exam/edit-exam.component.ts
@@ -5,6 +5,7 @@ import {Answer} from "../../models/answer";
 import {Exam} from "../../models/exam";
 import {Question} from "../../models/question";
 import {ActivatedRoute} from "@angular/router";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-edit-exam',
@@ -15,32 +16,52 @@ export class EditExamComponent {
 
   constructor(public examService: ExamsService, private dialogService: DialogService,
               private route: ActivatedRoute) {
-
     this.examId = this.route.snapshot.paramMap.get('id');
     this.examService.getNewExamData().subscribe(data => this.difficultyLevels = data);
     this.examService.getExamToEditById(this.examId).subscribe(data => {
-      this.examModel = data;
+      this.examNameControl.patchValue(data.examName);
+      this.difficultyLevelControl.patchValue(data.difficultyLevel);
+      this.examTimeControl.patchValue(this.getExamTimeStringFromNumber(data.timeInSeconds));
       this.examModel.questions = data.questions;
-      this.examHours = Math.floor(data.timeInSeconds / 3600)
-      this.examMinutes = (data.timeInSeconds % 3600) / 60;
     });
   }
 
-  examModel = new Exam();
   examId: any;
+  examModel = new Exam();
   newQuestionType = "single";
   difficultyLevels: any;
-  examHours!: number;
-  examMinutes!: number;
 
-  addNewQuestion(questionType: string) {
-    let quest = this.examService.getNewQuestion(questionType);
+  examNameControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(1),
+    Validators.maxLength(255)
+  ]);
+
+  difficultyLevelControl = new FormControl('', [
+    Validators.required,
+  ]);
+
+  examTimeControl = new FormControl('', [
+    Validators.required,
+    Validators.pattern("^\\d{2}:[0-5]\\d$"),
+  ]);
+
+  newExamForm = new FormGroup({
+    examName: this.examNameControl,
+    difficultyLevel: this.difficultyLevelControl,
+    examTime: this.examTimeControl,
+  });
+
+  addNewQuestion() {
+    let quest = this.examService.getNewQuestion(this.newQuestionType);
     this.examModel.questions.push(quest);
   }
 
   addNewAnswerToQuestion(question: Question) {
     let answer = new Answer(null, "", false);
-    if (question.type === 'short') answer.correctness = true;
+    if (question.type === 'short') {
+      answer.correctness = true;
+    }
     question.answers.push(answer);
   }
 
@@ -58,37 +79,109 @@ export class EditExamComponent {
     }
   }
 
-  uncheckOtherAnswers(answer: Answer, question: Question) {
+  postExam() {
+    if (this.newExamForm.valid
+      && this.doAllQuestionsContainContent()
+      && this.doAllAnswersContainContent()
+      && this.doQuestionsContainMinimalAnswersNumber()
+      && this.doQuestionsContainMinimalCorrectAnswersNumber()
+      && this.examTimeCalc() != 0) {
+      this.examModel.examName = this.examNameControl.value;
+      this.examModel.difficultyLevel = this.difficultyLevelControl.value;
+      this.examModel.timeInSeconds = this.examTimeCalc();
+      this.examService.postNewExam(this.examModel).subscribe(console.log);
+      alert("Egzamin został zapisany.");
+      window.location.reload();
+    } else {
+      if (!this.doAllAnswersContainContent())
+        alert("Wszystkie odpowiedzi muszą zawierać odpowiedź.");
+      else if (!this.doAllQuestionsContainContent())
+        alert("Wszystkie pytania muszą zawierać odpowiedź.")
+      else if (!this.doQuestionsContainMinimalAnswersNumber())
+        alert("Pytania z pisemną odpowiedzią muszą zawierać conajmniej jedną odpowiedź, pozostałe conajmniej dwie.");
+      else if (this.examTimeCalc() === 0)
+        alert("Czas egzaminu nie może być zerowy.");
+      else if (!this.doQuestionsContainMinimalCorrectAnswersNumber())
+        alert("Każde pytanie musi zawierać conajmniej jedną prawidłową odpowiedź.");
+      else
+        alert("Sprawdź poprawność wprowadzonych danych.");
+    }
+  }
+
+  doAllAnswersContainContent() {
+    let count = 0;
+    this.examModel.questions.forEach(question => {
+      question.answers.forEach(answer => {
+        if (answer.content.trim().length === 0) count++;
+      });
+    });
+    return count === 0;
+  }
+
+  doAllQuestionsContainContent() {
+    let count = 0;
+    this.examModel.questions.forEach(question => {
+      if (question.content.trim().length === 0) count++;
+    });
+    return count === 0;
+  }
+
+  doQuestionsContainMinimalAnswersNumber() {
+    let count = 0;
+    this.examModel.questions.forEach(question => {
+      if (question.type != 'short') {
+        if (question.answers.length < 1) count++;
+      } else {
+        if (question.answers.length < 2) count++;
+      }
+    });
+    return count === 0;
+  }
+
+  doQuestionsContainMinimalCorrectAnswersNumber() {
+    let count = 0;
+    this.examModel.questions.forEach(question => {
+      if (question.answers.filter(answer => answer.correctness).length == 0) count++;
+    });
+    return count == 0;
+  }
+
+  saveExam(information: string) {
+    const answer = this.dialogService.getDialog(information);
+    answer.afterClosed().subscribe(accept => {
+      if (accept) this.postExam();
+    });
+  }
+
+  examTimeCalc() {
+    if (this.examTimeControl.value.length != 5) {
+      return -1;
+    } else {
+      let hours = Number(this.examTimeControl.value.substring(0, 2));
+      let minutes = Number(this.examTimeControl.value.substring(3));
+      return (hours * 3600) + (minutes * 60);
+    }
+  }
+
+  selectAnswer(answer: Answer, question: Question) {
     if (question.type === 'single') {
       question.answers.forEach(answer => answer.correctness = false);
       answer.correctness = true;
     }
   }
 
-  postExam() {
-    this.examModel.timeInSeconds = this.getExamTime();
-    if (this.examService.validateExamName(this.examModel) &&
-      this.examService.validateDifficultyLevel(this.examModel) &&
-      this.examService.validateExamTimer(this.examHours, this.examMinutes) &&
-      this.examService.validateExamQuestions(this.examModel) &&
-      this.examService.validateExamTime(this.examModel)) {
-      this.examService.putExam(this.examModel).subscribe(console.log);
-      alert("Egzamin został nadpisany!");
-    } else alert("Nadpisywanie egzaminu nie powiodło się.");
-
-  }
-
-  private getExamTime() {
-    return (this.examHours * 3600) + (this.examMinutes * 60);
-  }
-
-  saveExam(information: string) {
-    const answer = this.dialogService.getDialog(information);
-
-    answer.afterClosed().subscribe(accept => {
-      if (accept) this.postExam();
-    });
-
+  getExamTimeStringFromNumber(seconds: number) {
+    let h = "00";
+    let min: string;
+    if (seconds >= 3600) {
+      h = Math.floor(seconds / 3600).toString();
+      min = Math.floor(((seconds % 3600) / 60)).toString();
+    } else {
+      min = Math.floor(((seconds % 3600) / 60)).toString();
+    }
+    if (h.length == 1) h = "0" + h;
+    if (min.length == 1) min = "0" + min;
+    return h + ":" + min;
   }
 
 }
