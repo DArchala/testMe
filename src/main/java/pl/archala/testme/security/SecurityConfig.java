6b02d8b64fa4c0af5cd0b109ca236e5e3c02ad85
damian <damianarchala@gmail.com>
2022-05-31 18:32:53 +0200

    Temporary permit all security

--- src/main/java/pl/archala/testme/security/SecurityConfig.java
+++ src/main/java/pl/archala/testme/security/SecurityConfig.java
@@ -2,19 +2,27 @@ package pl.archala.testme.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import pl.archala.testme.entity.UserDetailsServiceImpl;
+import pl.archala.testme.service.UserDetailsServiceImpl;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserDetailsServiceImpl userDetailsService;
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     public SecurityConfig(UserDetailsServiceImpl userDetailsService) {
         this.userDetailsService = userDetailsService;
@@ -27,14 +35,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+        http.cors().and().csrf().disable().headers().frameOptions().sameOrigin();
 
-        http.headers().disable();
+        http.authorizeRequests().antMatchers("/**").permitAll();
+//        http.authorizeRequests().antMatchers( "/api/register", "/api/login", "/api/logout").permitAll();
+//        http.authorizeRequests().antMatchers("/api/**").hasAnyRole("USER", "ADMIN");
 
-        http.authorizeRequests().antMatchers("/*").hasAnyRole("USER", "ADMIN");
+    }
 
-        http.formLogin().permitAll();
-
-        http.authorizeRequests().anyRequest().hasAnyRole("USER", "ADMIN");
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedHeaders("*").allowCredentials(true);
+            }
+        };
     }
 }
