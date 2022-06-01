ad018ba07219f1fdbd2ca4070db4bbd0cd03d5a8
damian <damianarchala@gmail.com>
2022-06-01 13:25:37 +0200

    Change login component ts, html, css

--- app-angular/src/app/pages/login/login.component.ts
+++ app-angular/src/app/pages/login/login.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {AuthenticationService} from "../../services/authentication.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  constructor(private authService: AuthenticationService, private router: Router) {
+  }
 
-  ngOnInit(): void {
+  usernameControl = new FormControl('', [
+    Validators.required,
+  ]);
+
+  passwordControl = new FormControl('', [
+    Validators.required,
+  ]);
+
+  userLoginForm = new FormGroup({
+    username: this.usernameControl,
+    password: this.passwordControl,
+  });
+
+  login() {
+    if (this.userLoginForm.invalid) {
+      alert("Sprawdź poprawność wpisanych informacji.");
+    } else {
+      this.authService.authenticate(this.usernameControl.value, this.passwordControl.value).subscribe(
+        data => {
+          this.router.navigate(["/home"]);
+        },
+        error => {
+          alert("Error = " + error);
+        }
+      );
+    }
   }
 
 }
