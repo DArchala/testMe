a6108d0ba7fa4dd78d0f2908c0c9936ebf235f37
damian <damianarchala@gmail.com>
2022-06-01 21:03:06 +0200

    Add app css html ts
    Add and update css headers styles
    
    Add id's and classes to html
    
    Add my-account, logout pages
    
    Add username in header
    
    Add logMeOut method
    
    Add elements to constructor

--- app-angular/src/app/app.component.ts
+++ app-angular/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {AuthenticationService} from "./services/authentication.service";
+import {DialogService} from "./services/dialog.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -9,8 +11,16 @@ import {AuthenticationService} from "./services/authentication.service";
 export class AppComponent {
   title = 'app-angular';
 
-  constructor(public authService: AuthenticationService) {
+  constructor(public authService: AuthenticationService, private dialogService: DialogService,
+              private router: Router) {
 
+  }
+
+  logMeOut(information: string) {
+    const answer = this.dialogService.getDialog(information);
+    answer.afterClosed().subscribe(accept => {
+      if (accept) this.router.navigate(['logout']);
+    });
   }
 
 }
