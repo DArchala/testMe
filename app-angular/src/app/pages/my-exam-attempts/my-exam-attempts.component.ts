44b9a7fc2fe47ea829ff8f9ef65bdd99e1d16d55
damian <damianarchala@gmail.com>
2022-06-05 18:20:41 +0200

    Implement my exam attempts sorting table

--- app-angular/src/app/pages/my-exam-attempts/my-exam-attempts.component.ts
+++ app-angular/src/app/pages/my-exam-attempts/my-exam-attempts.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MatTableDataSource} from "@angular/material/table";
+import {Sort} from "@angular/material/sort";
+import {LiveAnnouncer} from "@angular/cdk/a11y";
+import {ExamAttempt} from "../../models/exam-attempt";
 
 @Component({
   selector: 'app-my-exam-attempts',
   templateUrl: './my-exam-attempts.component.html',
   styleUrls: ['./my-exam-attempts.component.css']
 })
-export class MyExamAttemptsComponent implements OnInit {
+export class MyExamAttemptsComponent {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor(private _liveAnnouncer: LiveAnnouncer) {
   }
 
+  displayedColumns: string[] = ['id', 'examName', 'examTime', 'examUserTime', 'examUserPoints', 'examMaxPoints', 'startTimeDate', 'endTimeDate', 'examDifficultyLevel'];
+
+  ELEMENT_DATA: ExamAttempt[] = [
+    {id: 1, examName: 'Hydrogen', examTime: 1, examUserTime: 5, examUserPoints: 1 , examMaxPoints: 1, startTimeDate: new Date(), endTimeDate: new Date(), examDifficultyLevel: 'MEDIUM'},
+    {id: 2, examName: 'Helium',   examTime: 1, examUserTime: 5, examUserPoints: 1, examMaxPoints: 1, startTimeDate: new Date(), endTimeDate: new Date(), examDifficultyLevel: 'MEDIUM'},
+  ];
+
+  dataSource = new MatTableDataSource(this.ELEMENT_DATA);
+
+  // announceSortChange(sortState: Sort) {
+  //   if (sortState.direction)
+  //     this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+  //   else
+  //     this._liveAnnouncer.announce('Sorting cleared');
+  //
+  // }
 }
