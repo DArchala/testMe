4e8805ecf57fe883d6618b3d408741de5b5ef228
damian <damianarchala@gmail.com>
2022-06-05 11:16:23 +0200

    Implement ExamAttempt

--- src/main/java/pl/archala/testme/entity/ExamAttempt.java
+++ src/main/java/pl/archala/testme/entity/ExamAttempt.java
@@ -1,2 +1,44 @@
-package pl.archala.testme.entity;public class ExamAttempt {
+package pl.archala.testme.entity;
+
+import lombok.*;
+import pl.archala.testme.entity.abstractEntities.AbstractEntity;
+import pl.archala.testme.enums.ExamDifficultyLevel;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "exam_attempts")
+public class ExamAttempt extends AbstractEntity<Long> {
+
+    @ManyToOne
+    private User user;
+
+    private String examName;
+    private long examTime;
+    private long examUserTime;
+    private long examUserPoints;
+    private long examMaxPoints;
+    private LocalDateTime startTimeDate;
+    private LocalDateTime endTimeDate;
+    private ExamDifficultyLevel examDifficultyLevel;
+
+    public ExamAttempt(ExamForm examForm) {
+        this.examName = examForm.getExam().getExamName();
+        this.examTime = examForm.getExam().getTimeInSeconds();
+        this.examUserTime = examForm.getExamDateTime().getUserExamTime();
+        this.startTimeDate = examForm.getExamDateTime().getStartDateTime();
+        this.endTimeDate = examForm.getExamDateTime().getEndDateTime();
+        this.examDifficultyLevel = examForm.getExam().getDifficultyLevel();
+    }
+
 }
