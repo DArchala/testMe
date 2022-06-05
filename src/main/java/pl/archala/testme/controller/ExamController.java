58297dfbe55a765a669e356ca5a356446b1bbed2
damian <damianarchala@gmail.com>
2022-06-05 11:12:37 +0200

    Update ExamController
    Add CustomResponseEntities
    
    Add savingExamAttemptToUser

--- src/main/java/pl/archala/testme/controller/ExamController.java
+++ src/main/java/pl/archala/testme/controller/ExamController.java
@@ -5,12 +5,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.archala.testme.entity.Exam;
+import pl.archala.testme.entity.ExamForm;
 import pl.archala.testme.enums.ExamDifficultyLevel;
 import pl.archala.testme.repository.ExamRepository;
 import pl.archala.testme.service.ExamService;
 
+import java.security.Principal;
 import java.util.List;
 import java.util.Optional;
+
+import static pl.archala.testme.entity.CustomResponseEntity.*;
 
 @Slf4j
 @RestController
@@ -29,26 +33,30 @@ public class ExamController {
     @GetMapping("/exams")
     public ResponseEntity<?> getExams() {
         List<Exam> exams = examService.getAllExams();
-        if (exams.isEmpty()) return new ResponseEntity<>("No exams found", HttpStatus.NOT_FOUND);
+        if (exams.isEmpty()) return NO_EXAMS_FOUND;
         return new ResponseEntity<>(exams, HttpStatus.OK);
     }
 
     @PostMapping("/exams/exam")
-    public int checkExamCorrectness(@RequestBody Exam exam) {
-        return examService.countUserExamPoints(exam);
+    public ResponseEntity<?> checkExamCorrectness(@RequestBody ExamForm examForm, Principal principal) {
+        if (examService.saveExamAttemptToUser(examForm, principal.getName()))
+            return USER_DOES_NOT_EXIST;
+
+        int score = examService.countUserExamPoints(examForm.getExam());
+        return new ResponseEntity<>(score, HttpStatus.OK);
     }
 
     @GetMapping("/exams/exam/{id}")
     public ResponseEntity<?> getExamById(@PathVariable("id") Long id) {
         Optional<Exam> exam = examRepo.findById(id);
-        if (exam.isEmpty()) return new ResponseEntity<>("Exam does not exist", HttpStatus.NOT_FOUND);
+        if (exam.isEmpty()) return EXAM_DOES_NOT_EXIST;
         return new ResponseEntity<>(exam.get().setAllAnswersFalse(), HttpStatus.OK);
     }
 
     @PostMapping("/exams/exam/{id}")
     public ResponseEntity<?> getExamMaxPoints(@PathVariable("id") Long id) {
-        if(examRepo.findById(id).isEmpty())
-            return new ResponseEntity<>("Exam does not exist", HttpStatus.NOT_FOUND);
+        if (examRepo.findById(id).isEmpty())
+            return EXAM_DOES_NOT_EXIST;
         int examMaxPoints = examService.getMaxPossibleExamPoints(id);
         return new ResponseEntity<>(examMaxPoints, HttpStatus.OK);
     }
@@ -61,26 +69,26 @@ public class ExamController {
     @PostMapping("/new-exam/save")
     public ResponseEntity<?> saveNewExam(@RequestBody Exam exam) {
         if (examService.saveNewExam(exam)) return new ResponseEntity<>(exam, HttpStatus.OK);
-        else return new ResponseEntity<>("Saving exam failed", HttpStatus.BAD_REQUEST);
+        else return SAVING_EXAM_FAILED;
     }
 
     @GetMapping("/exams/edit/{id}")
     public ResponseEntity<?> getExamToEditById(@PathVariable("id") Long id) {
         Optional<Exam> exam = examRepo.findById(id);
-        if (exam.isEmpty()) return new ResponseEntity<>("Exam does not exist", HttpStatus.NOT_FOUND);
+        if (exam.isEmpty()) return EXAM_DOES_NOT_EXIST;
         return new ResponseEntity<>(exam.get(), HttpStatus.OK);
     }
 
     @PutMapping("/exams/edit")
     public ResponseEntity<?> putExam(@RequestBody Exam exam) {
-        if (examService.putExam(exam)) return new ResponseEntity<>("Exam saved", HttpStatus.OK);
+        if (examService.putExam(exam)) return EXAM_SAVED;
         return new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
     }
 
     @DeleteMapping("/exams/delete/{id}")
     public ResponseEntity<?> deleteExam(@PathVariable("id") Long id) {
-        if (examService.deleteExam(id)) return new ResponseEntity<>("Exam deleted", HttpStatus.OK);
-        return new ResponseEntity<>("Exam not found", HttpStatus.NOT_FOUND);
+        if (examService.deleteExam(id)) return EXAM_DELETED;
+        return EXAM_DOES_NOT_EXIST;
     }
 
 }
