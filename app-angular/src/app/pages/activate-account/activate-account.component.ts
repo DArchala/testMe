a568b89297d77ca0726a40e0f9b91d2fe9239dc8
damian <damianarchala@gmail.com>
2022-06-06 12:44:35 +0200

    Create token info switch-statement

--- app-angular/src/app/pages/activate-account/activate-account.component.ts
+++ app-angular/src/app/pages/activate-account/activate-account.component.ts
@@ -12,10 +12,29 @@ export class ActivateAccountComponent {
   constructor(private route: ActivatedRoute, private userService: UserService) {
     this.token = this.route.snapshot.paramMap.get('token');
     this.userService.confirmToken(this.token).subscribe(
-      data => {
-
+      () => {
       }, error => {
-        this.info = error.error.text;
+        console.log(error.error);
+        switch (error.error) {
+          case 'Your token has expired.':
+            this.info = "Twój token wygasł. Utwórz nowe konto w panelu rejestracji.";
+            break;
+          case 'Token does not exist.':
+            this.info = "Podany token nie istnieje.";
+            break;
+          case 'Token has no user.':
+            this.info = "Podany token nie ma przypisanego użytkownika.";
+            break;
+          default:
+            this.info = 'Nastąpił nieoczekiwany błąd.'
+        }
+        switch (error.error.text) {
+          case 'User account is now enable. You can log in.':
+            this.info = "Konto zostało aktywowane, możesz teraz się zalogować.";
+            break;
+          default:
+            return;
+        }
       }
     );
 
