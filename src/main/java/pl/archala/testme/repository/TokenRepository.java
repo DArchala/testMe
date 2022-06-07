38d10a1d0f6a0d877f329784835ddf4c438e1fd9
damian <damianarchala@gmail.com>
2022-06-07 20:36:51 +0200

    Add findAllByUserUsername method in TokenRepo

--- src/main/java/pl/archala/testme/repository/TokenRepository.java
+++ src/main/java/pl/archala/testme/repository/TokenRepository.java
@@ -4,10 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.archala.testme.entity.Token;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface TokenRepository extends JpaRepository<Token, Long> {
 
     Optional<Token> findByValue(String value);
+
+    List<Token> findAllByUserUsername (String username);
 }
