1344f28c08a02616d9dbd36d3a356deb367fac65
damian <damianarchala@gmail.com>
2022-06-07 17:22:23 +0200

    Add new response in CustomResponseEntity
    Add USER_DELETED
    
    Add DELETING_LAST_ADMIN_FORBIDDEN
    
    Add USER_SAVED

--- src/main/java/pl/archala/testme/component/CustomResponseEntity.java
+++ src/main/java/pl/archala/testme/component/CustomResponseEntity.java
@@ -11,6 +11,10 @@ public final class CustomResponseEntity {
     public static final ResponseEntity<?> USERNAME_ALREADY_TAKEN = new ResponseEntity<>("This username is already taken.", HttpStatus.BAD_REQUEST);
     public static final ResponseEntity<?> EMAIL_ALREADY_TAKEN = new ResponseEntity<>("This e-mail is already taken.", HttpStatus.BAD_REQUEST);
     public static final ResponseEntity<?> ACCOUNT_ENABLED = new ResponseEntity<>("User account is now enable. You can log in.", HttpStatus.OK);
+    public static final ResponseEntity<?> USER_DELETED = new ResponseEntity<>("User deleted.", HttpStatus.OK);
+    public static final ResponseEntity<?> DELETING_LAST_ADMIN_FORBIDDEN = new ResponseEntity<>("Deleting last admin is forbidden.", HttpStatus.FORBIDDEN);
+    public static final ResponseEntity<?> USER_SAVED = new ResponseEntity<>("User saved.", HttpStatus.OK);
+
 
     //EXAM
     public static final ResponseEntity<?> EXAM_DOES_NOT_EXIST = new ResponseEntity<>("Exam does not exist.", HttpStatus.NOT_FOUND);
