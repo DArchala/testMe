50740fd9fa583a963799daafc735d7adb348e702
damian <damianarchala@gmail.com>
2022-06-07 17:28:35 +0200

    Add new methods in UserService
    Add deleteUser method
    
    Add getAllAdmins method
    
    Add updateUserRole method
    
    Add activateAccount method
    
    Change int return values in registerUser method

--- src/main/java/pl/archala/testme/service/UserService.java
+++ src/main/java/pl/archala/testme/service/UserService.java
@@ -2,14 +2,19 @@ package pl.archala.testme.service;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import pl.archala.testme.entity.Token;
 import pl.archala.testme.entity.User;
 import pl.archala.testme.repository.TokenRepository;
 import pl.archala.testme.repository.UserRepository;
-import pl.archala.testme.entity.Token;
 
 import javax.mail.MessagingException;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
+
+import static pl.archala.testme.enums.RoleEnum.ADMIN;
 
 @Service
 public class UserService {
@@ -31,11 +36,11 @@ public class UserService {
 
     public int registerUser(User user) {
         if (userRepo.findByUsername(user.getUsername()).isPresent()) return 0;
-        if (userRepo.findByEmail(user.getEmail()).isPresent()) return -1;
+        if (userRepo.findByEmail(user.getEmail()).isPresent()) return 1;
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepo.save(user);
         sendToken(user);
-        return 1;
+        return 2;
     }
 
     private void sendToken(User user) {
@@ -63,4 +68,51 @@ public class UserService {
         return sb.toString();
     }
 
+    public int deleteUser(Long id) {
+        User user = userRepo.findById(id).orElse(null);
+        if (user == null) return 0;
+
+        if (user.getRole().equals(ADMIN) && getAllAdmins().size() == 1) return 1;
+
+        userRepo.delete(user);
+        return 2;
+    }
+
+    private List<User> getAllAdmins() {
+        return userRepo.findAll()
+                .stream()
+                .filter(u -> u.getRole().equals(ADMIN))
+                .collect(Collectors.toList());
+    }
+
+    public int updateUserRole(User newUser) {
+        if (newUser.isNew()) return 0;
+
+        User user = userRepo.findById(newUser.getId()).orElse(null);
+        if (user == null) return 0;
+
+        user.setRole(newUser.getRole());
+        userRepo.save(user);
+        return 1;
+    }
+
+    public int activateAccount(String value) {
+        Token token = tokenRepo.findByValue(value).orElse(null);
+        if (token == null) return 0;
+
+        if (token.getExpirationDate().isBefore(LocalDateTime.now())) {
+            tokenRepo.delete(token);
+            Optional<User> user = userRepo.findByUsername(token.getUser().getUsername());
+            if (user.isPresent() && !user.get().isEnabled()) userRepo.delete(user.get());
+            return 1;
+        }
+
+        User user = token.getUser();
+        if (user == null) return 2;
+
+        user.setEnabled(true);
+        userRepo.save(user);
+        tokenRepo.delete(token);
+        return 3;
+    }
 }
