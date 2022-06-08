c2bfa301d55d4e836992a6d2be1eda31a585e716
damian <damianarchala@gmail.com>
2022-06-08 12:01:27 +0200

    Update users.component
    Add logout after deleting our user account
    
    Add navigate to my-account after setting us as user
    
    Add getUserDTOFromUser method

--- app-angular/src/app/pages/users/users.component.ts
+++ app-angular/src/app/pages/users/users.component.ts
@@ -6,6 +6,7 @@ import {Router} from "@angular/router";
 import {DialogService} from "../../services/dialog.service";
 import {AcceptRole} from "../../models/accept-role";
 import {RoleEnum} from "../../models/role-enum";
+import {AuthenticationService} from "../../services/authentication.service";
 
 @Component({
   selector: 'app-users',
@@ -15,7 +16,7 @@ import {RoleEnum} from "../../models/role-enum";
 export class UsersComponent {
 
   constructor(private userService: UserService, private router: Router,
-              private dialogService: DialogService) {
+              private dialogService: DialogService, private authService: AuthenticationService) {
     this.userService.getUsers().subscribe(next => {
       next.forEach((user: User) => {
         this.usersList.push(user);
@@ -29,10 +30,9 @@ export class UsersComponent {
   }
 
   displayedColumns: string[] = ['id', 'username', 'email', 'role'];
-
   usersList: User[] = [];
-
   dataSource = new MatTableDataSource(this.usersList);
+  settingMyselfAsUser = false;
 
   openUserOptions(user: User) {
     const answer = this.dialogService.getUserOptionsDialog();
@@ -59,6 +59,10 @@ export class UsersComponent {
           switch (error.status) {
             case 200:
               alert(error.status + ": " + error.error.text);
+              if (user.username === this.authService.getUsername()) {
+                this.authService.clear();
+                this.router.navigate(['login']);
+              }
               window.location.reload();
               break;
             default:
@@ -72,25 +76,27 @@ export class UsersComponent {
 
   changeRole(user: User) {
     let roles: string[] = [];
+
     this.userService.getRoles().subscribe((data: RoleEnum[]) => {
       data.forEach(role => {
         roles.push(role.toString());
       });
     });
+
     const answer = this.dialogService.getUserNewRoleDialog("Wybierz nową rolę użytkownika: " + user.username, roles);
     answer.afterClosed().subscribe((accept: AcceptRole) => {
       if (accept.accept) {
-        user.role = accept.role;
-        this.userService.updateUserRole(user).subscribe(
+        let userDTO = this.getUserDTOFromUser(user);
+        userDTO.role = accept.role;
+        if (userDTO.username === this.authService.getUsername() && accept.role === 'USER') this.settingMyselfAsUser = true;
+
+        this.userService.updateUserRole(userDTO).subscribe(
           () => {
           }, error => {
-            switch(error.status) {
+            switch (error.status) {
               case 200:
                 alert(error.error.text);
-                this.router.navigate(['my-account']);
-                break;
-              case 403:
-                alert("Brak uprawnień.");
+                if (this.settingMyselfAsUser) this.router.navigate(['my-account']);
                 break;
               default:
                 alert("Error " + error.status + ": " + error.error);
@@ -99,5 +105,15 @@ export class UsersComponent {
           });
       } else return;
     });
+  }
+
+  private getUserDTOFromUser(user: User) {
+    let userDTO = new User();
+    userDTO.id = user.id;
+    userDTO.username = user.username;
+    userDTO.email = user.email;
+    userDTO.isEnabled = user.isEnabled;
+    userDTO.password = user.password;
+    return userDTO;
   }
 }
