22877394edd0a622227ac26dde82171aaa4d668c
damian <damianarchala@gmail.com>
2022-06-09 11:55:02 +0200

    Update UserController
    Change methods order
    
    Change endpoints
    
    Add default endpoint start as /api/users
    
    Add resetPassword method
    
    Change methods names
    
    Remove unused TokenRepo in constructor

--- src/main/java/pl/archala/testme/controller/UserController.java
+++ src/main/java/pl/archala/testme/controller/UserController.java
@@ -7,19 +7,17 @@ import org.springframework.web.bind.annotation.*;
 import pl.archala.testme.dto.PasswordChangeRequest;
 import pl.archala.testme.entity.User;
 import pl.archala.testme.enums.RoleEnum;
-import pl.archala.testme.repository.TokenRepository;
 import pl.archala.testme.repository.UserRepository;
 import pl.archala.testme.service.UserService;
 
 import javax.validation.Valid;
-import java.security.Principal;
 import java.util.List;
 
 import static pl.archala.testme.component.CustomResponseEntity.*;
 
 @Slf4j
 @RestController
-@RequestMapping(path = "/api", produces = "application/json")
+@RequestMapping(path = "/api/users", produces = "application/json")
 @CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
@@ -27,12 +25,62 @@ public class UserController {
 
     private final UserRepository userRepo;
 
-    private final TokenRepository tokenRepo;
-
-    public UserController(UserService userService, UserRepository userRepo, TokenRepository tokenRepo) {
+    public UserController(UserService userService, UserRepository userRepo) {
         this.userService = userService;
         this.userRepo = userRepo;
-        this.tokenRepo = tokenRepo;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> findAllUsers() {
+        List<User> users = userRepo.findAll();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    @PostMapping("/findBy/username")
+    public ResponseEntity<?> findUserByUsername(@RequestBody String username) {
+        User user = userRepo.findByUsername(username).orElse(null);
+        if (user != null) return new ResponseEntity<>(user, HttpStatus.OK);
+        else return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/update/role")
+    public ResponseEntity<?> updateUserRole(@RequestBody @Valid User user) {
+        switch (userService.updateUserRole(user)) {
+            case 0:
+                return USER_DOES_NOT_EXIST;
+            case 1:
+                return USER_SAVED;
+            case 2:
+                return DELETING_LAST_ADMIN_FORBIDDEN;
+            default:
+                return UNDEFINED_ERROR;
+        }
+    }
+
+    @PutMapping("/update/password")
+    public ResponseEntity<?> updateUserPassword(@RequestBody @Valid User user) {
+        switch (userService.updateNewPasswordUser(user)) {
+            case 0:
+                return USER_DOES_NOT_EXIST;
+            case 1:
+                return USER_SAVED;
+            default:
+                return UNDEFINED_ERROR;
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable("id") Long id) {
+        switch (userService.deleteUser(id)) {
+            case 0:
+                return USER_DOES_NOT_EXIST;
+            case 1:
+                return DELETING_LAST_ADMIN_FORBIDDEN;
+            case 2:
+                return USER_DELETED;
+            default:
+                return UNDEFINED_ERROR;
+        }
     }
 
     @PostMapping("/register")
@@ -49,9 +97,9 @@ public class UserController {
         }
     }
 
-    @GetMapping("/token")
+    @GetMapping("/activateAccount/token")
     public ResponseEntity<?> activateAccountByToken(@RequestParam String value) {
-        switch (userService.activateAccount(value)) {
+        switch (userService.activateAccountByToken(value)) {
             case 0:
                 return TOKEN_DOES_NOT_EXIST;
             case 1:
@@ -66,28 +114,15 @@ public class UserController {
 
     }
 
-    @PostMapping("/users/findBy/username")
-    public ResponseEntity<?> findUserByUsername(@RequestBody String username) {
-        User user = userRepo.findByUsername(username).orElse(null);
-        if (user != null) return new ResponseEntity<>(user, HttpStatus.OK);
-        else return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    }
-
-    @GetMapping("/users")
-    public ResponseEntity<?> findAllUsers() {
-        List<User> users = userRepo.findAll();
-        return new ResponseEntity<>(users, HttpStatus.OK);
-    }
-
-    @DeleteMapping("/users/delete/{id}")
-    public ResponseEntity<?> deleteUser(@PathVariable("id") Long id) {
-        switch (userService.deleteUser(id)) {
+    @GetMapping("/passwordReset/token")
+    public ResponseEntity<?> confirmPasswordResetByToken(@RequestParam String value) {
+        switch (userService.resetPasswordByToken(value)) {
             case 0:
-                return USER_DOES_NOT_EXIST;
+                return TOKEN_DOES_NOT_EXIST;
             case 1:
-                return DELETING_LAST_ADMIN_FORBIDDEN;
+                return TOKEN_HAS_EXPIRED;
             case 2:
-                return USER_DELETED;
+                return new ResponseEntity<>(userService.findUserByTokenValue(value), HttpStatus.OK);
             default:
                 return UNDEFINED_ERROR;
         }
@@ -98,22 +133,8 @@ public class UserController {
         return new ResponseEntity<>(RoleEnum.values(), HttpStatus.OK);
     }
 
-    @PutMapping("/users/update/role")
-    public ResponseEntity<?> updateUserRole(@RequestBody @Valid User user) {
-        switch (userService.updateUserRole(user)) {
-            case 0:
-                return USER_DOES_NOT_EXIST;
-            case 1:
-                return USER_SAVED;
-            case 2:
-                return DELETING_LAST_ADMIN_FORBIDDEN;
-            default:
-                return UNDEFINED_ERROR;
-        }
-    }
-
-    @PostMapping("users/changePassword")
-    public ResponseEntity<?> changePassword(@RequestBody @Valid PasswordChangeRequest passwordChangeRequest) {
+    @PostMapping("/changeMyPassword")
+    public ResponseEntity<?> changeMyPassword(@RequestBody @Valid PasswordChangeRequest passwordChangeRequest) {
         switch (userService.updatePassword(passwordChangeRequest)) {
             case 0:
                 return USER_DOES_NOT_EXIST;
@@ -125,6 +146,20 @@ public class UserController {
                 return NEW_PASSWORD_CANNOT_BE_EQUAL_TO_OLD_PASSWORD;
             case 4:
                 return USER_SAVED;
+            default:
+                return UNDEFINED_ERROR;
+        }
+    }
+
+    @PostMapping("/passwordResetRequest")
+    public ResponseEntity<?> resetPassword(@RequestBody String email) {
+        switch (userService.resetPassword(email)) {
+            case 0:
+                return USER_DOES_NOT_EXIST;
+            case 1:
+                return PASSWORD_RESET_LINK_WAS_SENT_CHECK_MAILBOX;
+            case 2:
+                return PASSWORD_RESETTING_FOR_DISABLED_USER_IS_FORBIDDEN;
             default:
                 return UNDEFINED_ERROR;
         }
