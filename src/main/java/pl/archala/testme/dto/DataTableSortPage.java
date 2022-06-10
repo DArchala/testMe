75040e121b617019a3be9649fba7887804faa08b
damian <damianarchala@gmail.com>
2022-06-10 18:41:22 +0200

    Implement DatTableSortPage

--- src/main/java/pl/archala/testme/dto/DataTableSortPage.java
+++ src/main/java/pl/archala/testme/dto/DataTableSortPage.java
@@ -1,2 +1,24 @@
-package pl.archala.testme.dto;public class DataTableSortPage {
+package pl.archala.testme.dto;
+
+import lombok.*;
+
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class DataTableSortPage {
+
+    private int page;
+    private int length;
+
+    @NotEmpty
+    private String column;
+
+    @NotEmpty
+    private String direction;
+
 }
