099910f7c362e679fa3df7a4bcbb87797827b02b
damian <damianarchala@gmail.com>
2022-06-13 11:22:49 +0200

    Add min token value size annotation

--- src/main/java/pl/archala/testme/entity/Token.java
+++ src/main/java/pl/archala/testme/entity/Token.java
@@ -1,12 +1,13 @@
 package pl.archala.testme.entity;
 
 import lombok.*;
-import pl.archala.testme.entity.User;
 import pl.archala.testme.entity.abstractEntities.AbstractEntity;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
-import java.time.LocalDate;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Getter
@@ -26,6 +27,7 @@ public class Token extends AbstractEntity<Long> {
     private User user;
 
     @NotEmpty
+    @Size(min = 10)
     private String value;
 
     private LocalDateTime expirationDate;
