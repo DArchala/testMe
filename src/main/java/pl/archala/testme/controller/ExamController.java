2b2484225a2013b9f034ff77aaaeab06b1059367
damian <damianarchala@gmail.com>
2022-06-14 13:56:43 +0200

    Change switch-case to try-catch in ExamController

--- src/main/java/pl/archala/testme/controller/ExamController.java
+++ src/main/java/pl/archala/testme/controller/ExamController.java
@@ -12,14 +12,13 @@ import pl.archala.testme.service.ExamService;
 
 import javax.validation.Valid;
 import java.security.Principal;
-import java.util.List;
 import java.util.Optional;
 
 import static pl.archala.testme.component.CustomResponseEntity.*;
 
 @Slf4j
 @RestController
-@RequestMapping(path = "/api", produces = "application/json")
+@RequestMapping(path = "/api/exams", produces = "application/json")
 @CrossOrigin(origins = "http://localhost:4200")
 public class ExamController {
 
@@ -31,34 +30,40 @@ public class ExamController {
         this.examService = examService;
     }
 
-    @GetMapping("/exams")
+    @GetMapping
     public ResponseEntity<?> getExams() {
-        List<Exam> exams = examService.getAllExams();
-        if (exams.isEmpty()) return NO_EXAMS_FOUND;
-        return new ResponseEntity<>(exams, HttpStatus.OK);
+        return new ResponseEntity<>(examService.getAllExams(), HttpStatus.OK);
     }
 
-    @PostMapping("/exams/exam")
+    @PostMapping("/exam")
     public ResponseEntity<?> checkExamCorrectness(@RequestBody @Valid ExamForm examForm, Principal principal) {
-        if (examService.saveExamAttemptToUser(examForm, principal.getName()))
-            return USER_DOES_NOT_EXIST;
-        int score = examService.countUserExamPoints(examForm.getExam());
-        return new ResponseEntity<>(score, HttpStatus.OK);
+        try {
+            examService.saveExamAttemptToUser(examForm, principal.getName());
+            int score = examService.countUserExamPoints(examForm.getExam());
+            return new ResponseEntity<>(score, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
     }
 
-    @GetMapping("/exams/exam/{id}")
-    public ResponseEntity<?> getExamById(@PathVariable("id") Long id) {
-        Optional<Exam> exam = examRepo.findById(id);
-        if (exam.isEmpty()) return EXAM_DOES_NOT_EXIST;
-        return new ResponseEntity<>(exam.get().setAllAnswersFalse(), HttpStatus.OK);
+    @GetMapping("/exam/{id}")
+    public ResponseEntity<?> findExamById(@PathVariable("id") Long id) {
+        try {
+            Exam exam = examService.findExamById(id);
+            return new ResponseEntity<>(exam, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
     }
 
-    @PostMapping("/exams/exam/{id}")
-    public ResponseEntity<?> getExamMaxPoints(@PathVariable("id") Long id) {
-        if (examRepo.findById(id).isEmpty())
-            return EXAM_DOES_NOT_EXIST;
-        int examMaxPoints = examService.getMaxPossibleExamPoints(id);
-        return new ResponseEntity<>(examMaxPoints, HttpStatus.OK);
+    @PostMapping("/exam/max-points")
+    public ResponseEntity<?> getExamMaxPoints(@RequestBody Long id) {
+        try {
+            int examMaxPoints = examService.getMaxPossibleExamPoints(id);
+            return new ResponseEntity<>(examMaxPoints, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
     }
 
     @GetMapping("/new-exam")
@@ -68,35 +73,32 @@ public class ExamController {
 
     @PostMapping("/new-exam/save")
     public ResponseEntity<?> saveNewExam(@RequestBody @Valid Exam exam) {
-        switch (examService.saveNewExam(exam)) {
-            case 0:
-                return EXAM_NAME_ALREADY_TAKEN;
-            case 1:
-                return EXAM_ANY_QUESTION_DOES_NOT_CONTAIN_ANY_CORRECT_ANSWER;
-            case 2:
-                return EXAM_SAVED;
-            default:
-                return SAVING_EXAM_FAILED;
+        try {
+            examService.saveNewExam(exam);
+            return new ResponseEntity<>("Exam saved.", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
 
-    @GetMapping("/exams/edit/{id}")
-    public ResponseEntity<?> getExamToEditById(@PathVariable("id") Long id) {
-        Optional<Exam> exam = examRepo.findById(id);
-        if (exam.isEmpty()) return EXAM_DOES_NOT_EXIST;
-        return new ResponseEntity<>(exam.get(), HttpStatus.OK);
-    }
-
-    @PutMapping("/exams/edit")
+    @PutMapping("/edit")
     public ResponseEntity<?> putExam(@RequestBody @Valid Exam exam) {
-        if (examService.putExam(exam)) return EXAM_SAVED;
-        return new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
+        try {
+            examService.putExam(exam);
+            return new ResponseEntity<>("Exam saved", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 
-    @DeleteMapping("/exams/delete/{id}")
+    @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> deleteExam(@PathVariable("id") Long id) {
-        if (examService.deleteExam(id)) return EXAM_DELETED;
-        return EXAM_DOES_NOT_EXIST;
+        try {
+            examService.deleteExam(id);
+            return new ResponseEntity<>("Exam deleted", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 
 }
