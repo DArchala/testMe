df46ee128533e6c2fad6570cd46bfc2b8d893081
damian <damianarchala@gmail.com>
2022-06-17 12:23:14 +0200

    Remove response entity constant

--- src/main/java/pl/archala/testme/controller/ExamAttemptController.java
+++ src/main/java/pl/archala/testme/controller/ExamAttemptController.java
@@ -14,8 +14,6 @@ import pl.archala.testme.repository.UserRepository;
 import java.security.Principal;
 import java.util.List;
 
-import static pl.archala.testme.component.CustomResponseEntity.USER_DOES_NOT_EXIST;
-
 @Slf4j
 @RestController
 @RequestMapping(path = "/api", produces = "application/json")
@@ -31,7 +29,7 @@ public class ExamAttemptController {
     @GetMapping("/examAttempts")
     public ResponseEntity<?> getMyExamAttempts(Principal principal) {
         User user = userRepo.findByUsername(principal.getName()).orElse(null);
-        if (user == null) return USER_DOES_NOT_EXIST;
+        if (user == null) return new ResponseEntity<>("User does not exist.", HttpStatus.BAD_REQUEST);
 
         List<ExamAttempt> myExamAttempts = user.getExamAttempts();
         return new ResponseEntity<>(myExamAttempts, HttpStatus.OK);
